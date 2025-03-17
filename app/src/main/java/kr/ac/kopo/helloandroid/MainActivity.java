9c031f778420b6037aff210d94cfe41164391fd4
wshin025 <wody0506@gmail.com>
2025-03-17 16:28:01 +0900

    EditText에 입력받아서 처리 결과 표시

--- app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package kr.ac.kopo.helloandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        Button btn1= (Button) findViewById(R.id.btn1);
+        TextView text1 = findViewById(R.id.text1);
+        EditText edit1=findViewById(R.id.editName);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"버튼을 클릭하셨네요~", Toast.LENGTH_LONG);
+                toast.show();
+                String name = edit1.getText().toString();
+                text1.setText(name + "입니다.");
+            }
         });
+
+//익명클래스 구현해서 객체 참조
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
